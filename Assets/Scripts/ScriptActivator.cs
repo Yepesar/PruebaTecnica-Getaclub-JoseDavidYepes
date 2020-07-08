f0a2842777305e8614f264154e998eef1f24dae6
LeJose <jdyepes@hotmail.com>
2020-07-08 17:46:01 -0500

    Time trial finish on finish line

---
+++ Assets/Scripts/ScriptActivator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptActivator : MonoBehaviour
+{
+    [SerializeField] private LapObject lapScript;
+    [SerializeField] private float activationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lapScript.enabled = false;
+        StartCoroutine(Activate());
+    }
+
+    private IEnumerator Activate()
+    {
+        yield return new WaitForSeconds(activationTime);
+        lapScript.enabled = true;
+    }
+}
