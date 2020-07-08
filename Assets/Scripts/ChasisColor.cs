3411894288579a6ab6689dfb8a37840dcef71e93
LeJose <jdyepes@hotmail.com>
2020-07-08 18:38:09 -0500

    Kart color customization

---
+++ Assets/Scripts/ChasisColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasisColor : MonoBehaviour
+{
+    [SerializeField] private Material red;
+    [SerializeField] private Material blue;
+    [SerializeField] private Material green;
+    [SerializeField] private SkinnedMeshRenderer chasisMesh;
+
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("ChasisColor"))
+        {
+            PlayerPrefs.SetInt("ChasisColor", 0);
+            PlayerPrefs.Save();
+        }
+
+        SetNewColor(PlayerPrefs.GetInt("ChasisColor"));
+    }
+
+    public void SetNewColor(int index)
+    {
+        if (index == 0)
+        {
+            chasisMesh.material = red;
+        }
+        else if (index == 1)
+        {
+            chasisMesh.material = blue;
+        }
+        else if (index == 2)
+        {
+            chasisMesh.material = green;
+        }
+
+        PlayerPrefs.SetInt("ChasisColor", index);
+        PlayerPrefs.Save();
+    }
+}
